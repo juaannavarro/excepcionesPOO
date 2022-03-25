e7ce74aec5f671b3674d05ed5ca2f08c5ff7aa0d
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-03-25 15:30:52 +0000

    retoque

--- clases/web.py
+++ clases/web.py
@@ -9,21 +9,21 @@ class web:
     def __init__(self, correo):
         self.correo = correo
 
-    def acceso(x):
+    def acceso(correoE):
         x = input("Introduce un correo para entrar: ")
-        print(x)
-        if x is None or x == "":
-            print("Introduce un correo electrónico")
-            raise EntradaIncorrecta
-        
-        elif re.search(".*@.*\..*", x) is None:
-            print("El correo debe de escribirse así: xxx@xx.xx")
-            raise DireccionCorrecta
+        while True:
+            if x is None or x == "":
+                print("Introduce un correo electrónico: ")
+                raise EntradaIncorrecta
+            
+            elif re.search(".*@.*\..*", x) is None:
+                print("El correo debe de escribirse así: xxx@xx.xx")
+                raise DireccionCorrecta
 
-        elif x == "xxx@xx.xx":
-            print("Cuenta bloqueada por acceso fallido ")
-            raise CuentaBloqueada
-        else:
-            print("Bienvenido, has entrado en la web")
-            return
-print(web.acceso("x"))
+            elif x == "xxx@xx.xx":
+                print("Cuenta bloqueada por acceso fallido ")
+                raise CuentaBloqueada
+            else:
+                print("Bienvenido, has entrado en la web")
+                return
+print(web.acceso("correoE"))
